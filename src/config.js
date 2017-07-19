85a57b8b4f6d3e864b15fbe0165cadacdcc49335
Alex Persaud <alex_persaud01@yahoo.com>
2017-07-19 16:53:22 -0400

    Create aws lib

--- src/config.js
+++ src/config.js
@@ -1,6 +1,8 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
   cognito: {
+    REGION: 'us-east-1',
+    IDENTITY_POOL_ID: 'us-east-1:787605fd-b735-4e46-b0e2-e78905ac5354',
     USER_POOL_ID : 'us-east-1_kQetcfwHf',
     APP_CLIENT_ID : '2vke1gi3e2tqnijlkpldp2ptau',
   }
