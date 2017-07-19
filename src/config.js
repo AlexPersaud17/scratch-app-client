c3823c7bdaee0a2f82512f091e73c27e0009f1af
Alex Persaud <alex_persaud01@yahoo.com>
2017-07-19 16:57:19 -0400

    api gateway

--- src/config.js
+++ src/config.js
@@ -1,5 +1,9 @@
 export default {
   MAX_ATTACHMENT_SIZE: 5000000,
+  apiGateway: {
+    URL: 'https://em2ybqsonh.execute-api.us-east-1.amazonaws.com/prod',
+    REGION: 'us-east-1',
+  },
   cognito: {
     REGION: 'us-east-1',
     IDENTITY_POOL_ID: 'us-east-1:787605fd-b735-4e46-b0e2-e78905ac5354',
